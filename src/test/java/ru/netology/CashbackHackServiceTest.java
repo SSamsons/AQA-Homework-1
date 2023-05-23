83799c22d83f6ebfce416342ac7d9ab13b01fd6f
Stanislav <people.ss@yandex.ru>
2023-05-23 21:09:58 +0300

    Add TestNG commit

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology;
+
+import com.sun.nio.sctp.Association;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldHowMuchToAddFirst() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHowMuchToAddSecond() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(1000);
+        int expected = 1000;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHowMuchToAddThird() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHowMuchToAddFourth() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(1);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHowMuchToAddFifth() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(0);
+        int expected = 1000;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldHowMuchToAddSixth() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(1001);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
