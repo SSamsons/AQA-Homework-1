a03fc1a0e561a6d419251776585f6d98c0b73a34
Stanislav <people.ss@yandex.ru>
2023-08-11 11:34:12 +0300

    Repair ServiceTest commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
         CashbackHackService cashback = new CashbackHackService();
 
         int actual = cashback.remain(1000);
-        int expected = 1000;
+        int expected = 0;
 
         Assert.assertEquals(actual, expected);
     }
